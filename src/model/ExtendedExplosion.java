00193b92978e23fe84e72a1eaabb9d1e4f05dac3
Tóth Bálint <bdykca@inf.elte.hu>
2024-03-26 14:06:42 +0000

    Change bomb image

--- src/model/ExtendedExplosion.java
+++ src/model/ExtendedExplosion.java
@@ -8,7 +8,7 @@ public class ExtendedExplosion extends PowerUp{
     public ExtendedExplosion(int x, int y) {
         super(x,y);
         try {
-            this.image = ImageIO.read(getClass().getResourceAsStream("/assets/images/extendedExplosion.jpg"));
+            this.image = ImageIO.read(getClass().getResourceAsStream("/assets/images/extendedExplosion.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
