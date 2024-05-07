84201ddf3dd7433a8c2dde8bd0103ee1333b538d
Izabella <da3t4h@inf.elte.hu>
2024-05-07 14:43:35 +0200

    Test commit for pipline

---
+++ src/test/java/org/ctrlaltdefeat/model/movingElements/PlayerTest.java
@@ -0,0 +1,19 @@
+package org.ctrlaltdefeat.model.movingElements;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getId() {
+        Assertions.assertEquals(true, true);
+    }
+
+    @Test
+    void getBombs() {
+        Assertions.assertEquals(true, true);
+    }
+}
